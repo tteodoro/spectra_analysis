ed12d630f3a36037803e40b8c957464f82ebc6de
tqteodoro <tiagoquevedo@iqsc.usp.br>
2019-01-25 16:43:47 +0100

    Normalize intensities before calculating overlap

--- spectra_manipulation.py
+++ spectra_manipulation.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec 17 10:26:32 2018
-
 @author: tteodoro
 """
 ############## Modules ###############
@@ -10,9 +8,19 @@ import numpy as np
 
 
 ############# Functions ##############
+def sim_IR (calc, obsv):
+    """Measures the likeness between two (e.g.,calculated and observed) 
+    IR spectra.
+    Eqn 1: J. Shen et al. / Spectrochimica Acta Part A 76 (2010) 418-422"""
+    Ico = overlap_integrals (calc, obsv)
+    Icc = overlap_integrals (calc, calc)
+    Ioo = overlap_integrals (obsv, obsv)
+    return Ico / (Icc + Ioo - Ico)
+
+
 def overlap_integrals (i, j):
-    """Calculates the (self and) overlap integrals of spectra i and j."""
-    
+    """Calculates the (self and) overlap integrals of intensities i and j.
+    Eqn 2: J. Shen et al. / Spectrochimica Acta Part A 76 (2010) 418-422"""
     if len(i) != len(j): 
         raise ValueError("Spectra i and j have different lengths.")
     else: 
@@ -22,40 +30,69 @@ def overlap_integrals (i, j):
     return overlap 
 
 
-def SimIR (calc, obsv):
-    """Given a spectral range (vmin to vmax) calculates the spectrum similarity
-    for measuring the likeness between calculated and observed IR spectra."""
+def broaden_spectrum(peaks, points, hw):
+    """(Lorentzian) Broadening of a spectrum (peaks) for a given frequency
+    range (points) given a certain half-width. Units are not considered here"""
+    freqs, intns = zip(*peaks)    # Slice to take out the peak positions
+    broad_intns  = np.zeros_like(points)
+    for freq, ints in zip(freqs,intns):
+        broad_intns += Lorentzian(points, freq, hw) * ints
+    return broad_intns
 
-    Ico = overlap_integrals (calc, obsv)
-    Icc = overlap_integrals (calc, calc)
-    Ioo = overlap_integrals (obsv, obsv)
-    
-    return Ico / (Icc + Ioo - Ico)
+
+def Lorentzian(x,x0,hw):
+    """Lorentzian line shape function.
+    math:: L(x) = \frac{1}{\pi} \alpha \frac{1}{(x-x_0)^2 + \alpha^2}"""
+    return (1/np.pi) * hw / ((x - x0)**2 + hw**2)
+
+
+def normalize(v):
+    """Normalizes a given vector v"""
+    norm = np.linalg.norm(v)
+    if norm == 0: 
+       return v
+    return v / norm
     
 
 ############# Input #################
-vmin = 1000.
-vmax = 2500.    
+# If defalut (0 & 0), the range is determined by the frequency range in the
+# observed spectrum
+vmin = 0  #Beginning of frequency range
+vmax = 0  #End of frequency range
+# Path for the calculated and observed files
+calc_path = "ir_BLYP_DZ.xy"
+obsv_path = "ir_BP86_DZ.xy"  
+hw = 4.     # Half-width
+resol = 1.  # Resolution of broadened spectra in n_points/cm-1
     
 
-############ External ##############
-calc = np.loadtxt("calc.txt")
-obsv = np.loadtxt("obsv.txt")
-
+############ Preparing data ##############
+# Loads data
+calc = np.loadtxt(calc_path)
+obsv = np.loadtxt(obsv_path)
+# Unzip data
 calc_freq, calc_ints = zip(*calc)
 obsv_freq, obsv_ints = zip(*obsv)
 
-calc_ints = [x / 10 for x in calc_ints]
-
-selec_ints = [i for i in range(len(calc_ints)) \
-              if (calc_freq[i]>=vmin and calc_freq[i]<=vmax)]
-
-calc_selec_ints = calc_ints[selec_ints[0]:selec_ints[-1]]
-obsv_selec_ints = obsv_ints[selec_ints[0]:selec_ints[-1]]
+# Generates a new grid of frequencies based on observed data or input 
+if not vmin and not vmax:
+    broad_freqs = np.arange(obsv_freq[0],obsv_freq[-1],1/resol)
+else:
+    broad_freqs = np.arange(vmin,vmax,1/resol)
+#Re-broadens both spectra for consistency
+broad_cints = broaden_spectrum(calc, broad_freqs, hw)
+broad_oints = broaden_spectrum(obsv, broad_freqs, hw)
+#Normalize intensities
+broad_cints = normalize(broad_cints)
+broad_oints = normalize(broad_oints)
 
 
 ############ Running ###############
-simil = SimIR(calc_selec_ints, obsv_selec_ints)
+sim = sim_IR(broad_cints, broad_oints)
+
+
+############ Output ################
+print('S = {:>4.2f}'.format(sim))
 
 
 
